2f8da3f9e7cc3313fca2e2fbb14208e596369050
Baehyeonu <wea3113@gmail.com>
2025-03-24 18:28:28 +0900

    docs:Chapter_08 환경변수 관리(SECRET_KEY)

--- pystagram/config/settings.py
+++ pystagram/config/settings.py
@@ -9,18 +9,21 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import json
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+with open(BASE_DIR / '.config_secret'/ 'secret.json') as f:
+    config_secret_str = f.read()
 
+SECRET = json.loads(config_secret_str)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&m^2el2(6$rn#kgslz1ddybqf(iac-0frs72$j9n19f_r82uz^"
+SECRET_KEY = SECRET['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -137,3 +140,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Auth
 AUTH_USER_MODEL = 'member.User'
+
