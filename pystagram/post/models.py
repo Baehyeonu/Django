167ed52f7eadec586630b241a7b13fdf2e0f2972
Baehyeonu <wea3113@gmail.com>
2025-03-27 15:53:05 +0900

    feat:Chapter_09 댓글,태그 모델 만들기

--- pystagram/post/models.py
+++ pystagram/post/models.py
@@ -6,7 +6,7 @@ from utils.models import TimestampModel
 
 User = get_user_model()
 
-
+#글 생성
 class Post(TimestampModel):
     content = models.TextField('본문')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -18,7 +18,7 @@ class Post(TimestampModel):
         verbose_name = '포스트'
         verbose_name_plural = '포스트 목록'
 
-
+#이미지 생성
 class PostImage(TimestampModel):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='images') # 해당 이미지를 소유한 포스트를 나타내는 외래 키 필드로, Post 모델과 관계를 맺습니다.
     image = models.ImageField('이미지', upload_to='post/%Y/%m/%d')
@@ -29,3 +29,20 @@ class PostImage(TimestampModel):
     class Meta:
         verbose_name = '이미지'
         verbose_name_plural = '이미지 목록'
+
+#태그
+class Tag(TimestampModel):
+    tag = models.CharField('태그', max_length=100)
+    posts = models.ManyToManyField(Post, related_name='tags')
+
+    def __str__(self):
+        return self.tag
+    
+#댓글
+class Comment(TimestampModel):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    content = models.CharField('내용', max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.post} | {self.user}'
